23a52d791b4c04241bb944a0cc4141ae4e603f5d
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-05-02 21:39:21 +0900

    feat: Init project

---
+++ src/test/java/com/cleancode/houseutils/HouseutilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleancode.houseutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseutilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
