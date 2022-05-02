f7d12ab71c2da88ac94a091274a9b906905585a6
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2022-05-02 22:01:20 +0900

    feat: Day2 예제 코드

---
+++ src/main/java/com/cleancode/houseutils/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.cleancode.houseutils.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/api/ping")
+    public String ping() {
+        return "ok";
+    }
+}
